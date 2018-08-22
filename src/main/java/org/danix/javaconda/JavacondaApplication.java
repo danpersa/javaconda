f369c2c1ff9bb59681dd51cbfeccd2b53d4ed5e0
Dan Persa <dan.persa@gmail.com>
2018-08-23 00:06:39 +0200

    Initial commit

---
+++ src/main/java/org/danix/javaconda/JavacondaApplication.java
@@ -0,0 +1,12 @@
+package org.danix.javaconda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacondaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacondaApplication.class, args);
+	}
+}
